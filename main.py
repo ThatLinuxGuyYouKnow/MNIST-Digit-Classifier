d7b57dab7afedeee84ae7f6555582d61942d65c7
Emmanuels-code <arocket04@gmail.com>
2025-03-06 11:19:38 +0100

    added main code with training

---
+++ main.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# Load dataset
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0  # Normalize
+
+# Define a simple CNN
+model = keras.Sequential([
+    layers.Reshape((28, 28, 1), input_shape=(28, 28)),
+    layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+    layers.MaxPooling2D(pool_size=(2, 2)),
+    layers.Flatten(),
+    layers.Dense(128, activation="relu"),
+    layers.Dense(10, activation="softmax")
+])
+
+# Compile
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+# Train
+model.fit(x_train, y_train, epochs=5, batch_size=32, validation_data=(x_test, y_test))
