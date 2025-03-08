0164e85c2b4eaaabdf465bbd18a038066a209e14
Emmanuels-code <arocket04@gmail.com>
2025-03-08 13:37:10 +0100

    feature: moooreee layerssscd cnn_model

--- main.py
+++ main.py
@@ -13,11 +13,20 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 model = keras.Sequential([
     layers.Reshape((28, 28, 1), input_shape=(28, 28)),
     layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+    layers.BatchNormalization(),
+    layers.MaxPooling2D(pool_size=(2, 2)),
+    layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+    layers.BatchNormalization(),
+    layers.MaxPooling2D(pool_size=(2, 2)),
+    layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+    layers.BatchNormalization(),
     layers.MaxPooling2D(pool_size=(2, 2)),
     layers.Flatten(),
     layers.Dense(128, activation="relu"),
+    layers.Dropout(0.5),
     layers.Dense(10, activation="softmax")
 ])
+
 
 # Compile model
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
